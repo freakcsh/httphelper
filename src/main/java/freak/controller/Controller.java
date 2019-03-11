4ab152c22f329f87726025394f688089815bc666
kayisheng <3024768596@qq.com>
2019-03-11 18:49:44 +0800

    添加公共参数验证

--- src/main/java/freak/controller/Controller.java
+++ src/main/java/freak/controller/Controller.java
@@ -2,22 +2,32 @@ package freak.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import freak.common.Result;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
 @RestController
 public class Controller {
 
 
-    @RequestMapping("/login")
-    public Object login(String userName, String pwd) {
-        return toData(userName, pwd);
-    }
+//    @RequestMapping("/login")
+//    public Object login(String userName, String pwd) {
+//        return toData(userName, pwd);
+//    }
 
-    public Result toData(String userName, String pwd) {
+    @RequestMapping("/login")
+    public Object login(String lversion,String token,String userName, String pwd) {
+        org.slf4j.Logger logger=LoggerFactory.getLogger(Controller.class);
+        logger.debug("lversion"+lversion);
+        logger.debug("token"+token);
+        return toData(lversion,token,userName, pwd);
+    }
+    public Result toData(String lversion,String token,String userName, String pwd) {
         Result result = new Result();
         if (!"freak".equals(userName)) {
             result.setCode(48);
@@ -27,8 +37,6 @@ public class Controller {
             return result;
         } else if (!pwd.equals("123456")) {
             result.setCode(4000);
-//            Map map = new HashMap();
-//            result.setData(map);
             result.setData("");
             result.setMsg("密码错误");
             return result;
@@ -38,8 +46,38 @@ public class Controller {
             Map map = new HashMap();
             map.put("userName", userName);
             map.put("pwd", pwd);
+            map.put("abc", "");
+            map.put("token",token);
+            map.put("lversion",lversion);
             result.setData(map);
             return result;
         }
     }
+
+    @RequestMapping("/api/notToken")
+    public Object text() {
+        return toText();
+    }
+
+    private Result toText() {
+        Result result = new Result();
+        result.setCode(200);
+        result.setData("");
+        result.setMsg("测试成功");
+        return result;
+    }
+
+    @RequestMapping("/api/error")
+    public Object error() {
+        return toError();
+    }
+
+    private Result toError() {
+        Result result = new Result();
+        result.setCode(200);
+        result.setData("data");
+        result.setMsg("测试失败");
+        return result;
+    }
+
 }
---
+++ src/main/java/freak/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package freak.mapper;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public interface UserMapper {
+
+
+    Object xx();
+}
